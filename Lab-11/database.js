7babf51125269d96c100c591fe07c5e4419ecbcb
Badsha1996 <badshalaskar96@outlook.com>
2026-09-22 02:18:11 +0530

    databases

--- Lab-11/database.js
+++ Lab-11/database.js
@@ -13,14 +13,3 @@
 //     submitted BOOLEAN DEFAULT FALSE
 // );
 
-const { Pool } = require("pg");
-
-const pool = new Pool({
-  host: "localhost",
-  port: 5432,
-  user: "postgres",
-  password: ``,
-  database: "postgres",
-});
-
-module.exports = pool;
