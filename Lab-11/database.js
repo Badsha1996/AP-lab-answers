885d91d3d5ce0d2523ced1e5ec757dbc336cf1a2
Badsha1996 <badshalaskar96@outlook.com>
2026-09-24 01:00:12 +0530

    Lab 11 & 12

--- Lab-11/database.js
+++ Lab-11/database.js
@@ -13,3 +13,16 @@
 //     submitted BOOLEAN DEFAULT FALSE
 // );
 
+
+
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  host: "localhost",
+  port: 5432,
+  user: "postgres",
+  password: ``,
+  database: "postgres",
+});
+
+module.exports = pool;
--- Lab-11/index.js
+++ Lab-11/index.js
@@ -4,20 +4,74 @@ const app = express();
 
 app.use(express.json());
 
-// POST   /assignments
+// Create a record using POST /assignments,
 // body = {
 //     "title":"Backend Lab",
 //     "deadline":"2026-08-10"
 // }
+app.post("/assignments", async (req, res) => {
+  const { title, deadline } = req.body;
 
-// GET    /assignments
+  const result = await pool.query(
+    `INSERT INTO assignments
+        (title, deadline)
+        VALUES ($1,$2)
+        RETURNING *`,
 
-// PATCH  /assignments/:id
+    [title, deadline],
+  );
 
-// DELETE a record using PATCH /assignments
+  res.status(201).json(result.rows[0]);
+});
 
-// GET    /assignments?submitted=true
+// Fetch all record using GET /assignments
+app.get("/assignments", async (req, res) => {
+  const result = await pool.query("SELECT * FROM assignments");
+  res.json(result.rows);
+});
+
+// Update a record using PATCH /assignments
+app.patch("/assignments/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const result = await pool.query(
+    `UPDATE assignments
+        SET submitted=true
+        WHERE id=$1
+        RETURNING *`,
+
+    [id],
+  );
+
+  res.json(result.rows[0]);
+});
+
+// Delete a record using PATCH /assignments
+app.delete("/assignments/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const result = await pool.query(
+    `DELETE FROM assignments
+         WHERE id = $1
+         RETURNING *`,
+
+    [id],
+  );
+
+  if (result.rows.length === 0) {
+    return res.status(404).json({
+      message: "Assignment not found",
+    });
+  }
+
+  res.json({
+    message: "Assignment deleted successfully",
+    assignment: result.rows[0],
+  });
+});
+
 
 app.listen(3000, () => {
   console.log("The server is running 😀");
 });
+
